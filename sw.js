e84794e02e7219106d6287609a1a7d0417517838
andyandmiranda <andyundmiranda@gmail.com>
2026-07-20 13:10:21 +0900

    Repair complete Meowde v4.22 service worker

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
 const CACHE_NAME='meowde-v422-shell';
 const CORE_ASSETS=[
-  '/',
-  '/v412.html',
-  '/index.html',
-  '/manifest.webmanifest',
+  '/','/v412.html','/index.html','/manifest.webmanifest',
   '/v412-hotfix.js',
   '/v413-core.js','/v413-lesson.js','/v413-ux.css',
   '/v414-state.js','/v414-reward.js','/v414-screens.js','/v414-ux.css',
@@ -13,4 +10,5 @@ const CORE_ASSETS=[
   '/v418-coach.js','/v418-coach.css',
   '/v419-achievements.js','/v419-achievements.css',
   '/v420-profile.js','/v420-profile.css',
-  '/v421-vault.js
+  '/v421-vault.js','/v421-vault.css',
+  '/v422-pwa.js','/v422-pwa.css
